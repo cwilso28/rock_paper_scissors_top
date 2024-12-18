24ff9967c3a565bbffe6dd79f16e20f913920b25
Chris <47066922+cwilso28@users.noreply.github.com>
2024-12-17 21:14:15 -0600

    Add pseudocode for the random computer choice
    The computer should choose rock, paper, or scissors at random.

---
+++ script.js
@@ -0,0 +1,4 @@
+// Produce an option of rock, paper or scissors
+// COMPUTE a random number between 0 and 2, inclusive
+// GET an option from a list with the random number
+// PRINT the output to the console 
