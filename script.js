56a6eeec8ca85abdf001cc9d79fb0172f4d13857
Chris <47066922+cwilso28@users.noreply.github.com>
2024-12-17 21:29:10 -0600

    Create pseudocode for the human choice
    The function for the human code prompts the user for input and
    then check to see if the input is in the correct form

--- script.js
+++ script.js
@@ -12,4 +12,16 @@ function getComputerChoice() {
 
 // console.log(getComputerChoice());
 
-//
+// Ask the user for their choice
+// INIT a function to get the users choice
+function getHumanChoice() {
+    // INIT a variable for the users choice and prompt them to input their choice
+    // IF the user inputs a 1 or rock
+        // Display the message: "You chose rock"
+    // ELSE IF the user inputs a 2 or paper
+        // Display the message: "You chose paper"
+    // ELSE IF the user inputs a 3 or scissors
+        // Display the message: "You chose scissors"
+    // ELSE
+        // Display the message: "Incorrect input. Try again."
+}
